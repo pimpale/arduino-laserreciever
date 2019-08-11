1ef6cfb2d7bbca3611c2cc20dd90f4958a5e43e2
gpi <gpimpale29@gmail.com>
2019-08-10 22:37:25 -0700

    works

--- arduino-laserreciever.ino
+++ arduino-laserreciever.ino
@@ -1,22 +1,46 @@
-const int ledPin = 3;    // LED connected to digital pin 13
-const char* wordThing = "Yes";
+#define readReciever() (!digitalRead(7))
 
 void setup() {
-  pinMode(ledPin, OUTPUT);    // sets the digital pin as output
-}
+  Serial.begin(9600);
+  pinMode(7,INPUT);
 
-void loop() {
-  for(int i = 0; i < strlen(wordThing); i++) {
-    unsigned char byteThing = wordThing[i];
-    out_byte(byteThing);
+  // bitcounter is the bit we are currently on
+  uint8_t bitCounter = 0; 
+  uint8_t currentByte = 0;
+
+  uint8_t currentStatus = LOW;
+
+  int zcount = 0;
+  while(true) {
+    int status = readReciever();
+    if(status ==0) {
+      zcount++;
+      if(zcount == 16) {
+        break;
+      }
+    } else {
+      zcount == 0;
+    }
   }
   
+  while(true) {
+    delay(100);
+    //set current status
+    currentStatus = readReciever();
+
+    currentByte |= (currentStatus==HIGH) << bitCounter; 
+    bitCounter++;
+    // if bit counter has reached byte maximum, print it out and clear it
+    //Serial.println(currentStatus);
+    if(bitCounter == 8) {
+      Serial.println((char)currentByte);
+      currentByte=0;
+      bitCounter=0;
+    }
+  }
 }
 
-void out_byte(unsigned char out) {
-  
-  digitalWrite(ledPin, HIGH);
-  delayMicroseconds(out);
-  digitalWrite(ledPin, LOW);
-  delayMicroseconds(256 - out);
-}
+
+
+// loop is too slow
+void loop() {}
